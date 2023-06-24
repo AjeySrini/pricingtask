fb5b8ae54e672481702c610c0b08b0a5f5157b2d
Ajey <ajeysrini@gmail.com>
2023-06-24 18:37:42 +0530

    pricingtask

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -29,7 +28,7 @@ function App() {
                 Reports</li>
             </ul>
             <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+              <p  class="btn btn-primary text-uppercase">Button</p>
             </div>
           </div>
         </div>
@@ -53,7 +52,7 @@ function App() {
                 Reports</li>
             </ul>
             <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+              <p class="btn btn-primary text-uppercase">Button</p>
             </div>
           </div>
         </div>
@@ -77,7 +76,7 @@ function App() {
               <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
             </ul>
             <div class="d-grid">
-              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+              <p class="btn btn-primary text-uppercase">Button</p>
             </div>
           </div>
         </div>
